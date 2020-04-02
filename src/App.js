b4826248694b20047e5909083423233065b80c7e
laurakirkham <kirkham.laura@hotmail.com>
2020-04-01 21:31:53 -0400

    added cart component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,62 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import db from './db.js'
 
 import Banner from './components/Banner.js'
+import Product from "./components/Product.js"
+import Cart from "./components/Cart.js"
 
-function App() {
-  return (
-    <div className="App">
-      <Banner banner={db.banner}/>
-    </div>
-  );
+class App extends Component {
+  //specify you want to construct using props
+  constructor(props){
+    //super ensures Componenet is properly constructed before things are defined on this instance of App
+    //good idea to pass in  props - but wont error because as soon as super runs, will auto add it. Add just to be safe.
+    super(props)
+    // DEFINING STATE
+    //this refers to a rendered instance of App
+    this.state = {
+      //store data from db on state
+      ...db, 
+      lineItems: [],
+      cartOpen: false,
+
+    }
+  }
+
+  toggleCart = () => {
+    const cartNewState = !this.state.cartOpen
+    this.setState({
+      cartOpen: cartNewState
+    }, () => {console.log('updated!')})
+  }
+
+  addToCart = (product) => {
+    this.setState({
+      lineItems: [...this.state.lineItems, product]
+    })
+  }
+
+
+  render(){
+    return(
+      <div className="App">
+        {/* this is used below because its a method on THIS app instance */}
+        <span onClick={this.toggleCart}>Cart (0)</span>
+        {/* if what is on left side of && is truthy, render whats on the right */}
+        {this.state.cartOpen &&
+          <Cart />
+        }
+        <Banner banner={db.banner}/>
+        {/* use arrow function that utlizes an implicit return - remove curly braces and "return" */}
+        {db.products.map((product, index) => 
+          //map through each item, 
+          //create a new array of product components with a product passed in as props.
+          <Product product={product} addToCart={this.addToCart}/>
+        )}
+  
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Product.js
+++ src/components/Product.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 const Product = (props) => {
     const { imgUrl, title, price} = props.product;
+    const {addToCart} = props;
     return(
         <div>
             <img src={imgUrl} alt={title}/>
             <h2>{title}</h2>
             <p>{price}</p>
+            {/* below use an anonymous callback function to return addtoCart so its not called immediately */}
+            <button onClick={() => addToCart(props.product)}>Add to Cart</button>
         </div>
     )
 }
